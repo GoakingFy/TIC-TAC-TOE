eca86a14b7c6ad71a8adc9f080150727017d0c7f
goakingFy <joaquindiaz66@gmail.com>
2023-05-10 00:57:21 +0200

    added new features

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const cells = document.querySelectorAll(".cell")
+const x_turn = document.querySelector(".container-player-x-turn");
+const o_turn = document.querySelector(".container-player-o-turn");
+const modal = document.querySelector(".container-modal")
+const btnReset = document.querySelector(".btn-resetGame")
+const last10W = document.querySelector(".last-10-win")
+let seg = 0
+let min = 0
+let timer = document.querySelector(".timer");
+
+
+
 let winner = undefined
 const PLAYERS = {
     X: "X",
     O:"O"
 }
 
-const TABLE = ["","","","","","","","",""]
+let historyWin = []
+
+console.log(historyWin)
+
+let TABLE = ["","","","","","","","",""]
 const COMB_WIN = [ 
     [0,1,2],
     [3,4,5],
@@ -23,6 +38,8 @@ const COMB_WIN = [
 const INITIAL_PLAYER = PLAYERS.X;
 let current_player = INITIAL_PLAYER;
 
+timerGame()
+
 cells.forEach(cell => {
     
     
@@ -32,11 +49,15 @@ cells.forEach(cell => {
             
             let cell_content = e.target;
             let cell_id = cell_content.getAttribute("data-cell")
-            cell.classList.toggle(current_player);
-            cell_content.innerHTML = current_player;
-            TABLE[cell_id - 1] = current_player;
+          
+            x_turn.classList.toggle('select-turn')
+            o_turn.classList.toggle('select-turn')
            
-            
+            TABLE[cell_id - 1] = current_player;
+            cell_content.innerHTML = TABLE[cell_id - 1];
+            cell.classList.toggle(TABLE[cell_id - 1]);
+           
+            console.log(TABLE)
             change_player(PLAYERS)
             setWinner(COMB_WIN)
 
@@ -57,11 +78,18 @@ function setWinner(COMB_WIN){
        if(TABLE[COMB_WIN[i][0]] == "X" && TABLE[COMB_WIN[i][1]] == "X" && TABLE[COMB_WIN[i][2]] == "X"){
            winner = PLAYERS.X
            console.log("Ha ganado X")
+           displayModal(winner)
+           addHistoryWinner()
+           
+
+           
        } 
        
        if(TABLE[COMB_WIN[i][0]] == "O" && TABLE[COMB_WIN[i][1]] == "O" && TABLE[COMB_WIN[i][2]] == "O"){
            winner = PLAYERS.O
            console.log("Ha ganado O")
+           displayModal(winner)
+           addHistoryWinner()
        }
 
    
@@ -74,9 +102,83 @@ function setWinner(COMB_WIN){
     
 }
 
+btnReset.addEventListener("click" , ()=>{
+    modal.style.display = "none"
+    resetGame()
+})
 
 function resetGame(){
+    
+    cells.forEach((cell)=>{
+        let cell_id = cell.getAttribute("data-cell")
+        console.log(cell_id)
+        cell.innerHTML = ''
+        if(TABLE[cell_id -1]!= ""){
+            cell.classList.remove(TABLE[cell_id -1] );
+        }
+       
+    })
     TABLE = ["","","","","","","","",""]
     current_player = INITIAL_PLAYER;
+    
+    winner = undefined
+    x_turn.classList.add('select-turn')
+    o_turn.classList.remove('select-turn')
+    min = 0
+    seg = 0
+    console.log(historyWin)
+    loadHistoryWinner(historyWin)
+
+}
+
+
+function displayModal(winner){
+    const infoWinner = document.querySelector(".info-winner")
+    infoWinner.innerHTML = `Ha ganado el jugador ${winner}`
+
+  modal.style.display = "flex"  
+    
+
+}
+
+function timerGame() {
+   
+    
+      
+        setInterval(()=>{
+          seg++
+
+            if(seg == 60){
+            seg = 0
+            min++
+          }
+          
+          timer.innerHTML = `${min}:${seg}`
+          
+        
+        
+        },1000)
+       
+
+}
+
+function addHistoryWinner(){
+    let infoGame = {
+        winner,
+        time: timer.innerHTML
+    }
+
+    historyWin.push(infoGame)
+}
+
+function loadHistoryWinner(arr){
+    last10W.innerHTML = ``
+    arr.forEach( record =>{
+        console.log(record.winner)
+        let newElement =  document.createElement("p")
+        newElement.textContent = record.winner
+        newElement.classList.add(record.winner)
+        last10W.appendChild(newElement)
+    } )
 }
 
